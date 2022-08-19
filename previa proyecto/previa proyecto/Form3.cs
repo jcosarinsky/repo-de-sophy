f5893f76d964d2800e591f36570acd32ddef13ee
jcosarinsky <110557427+jcosarinsky@users.noreply.github.com>
2022-08-19 14:43:32 -0300

    agregamos tercer form

--- previa proyecto/previa proyecto/Form3.cs
+++ previa proyecto/previa proyecto/Form3.cs
@@ -10,29 +10,17 @@ using System.Windows.Forms;
 
 namespace previa_proyecto
 {
-    public partial class Form2 : Form
+    public partial class Form3 : Form
     {
-        public Form2(string Mensaje)
+        public Form3(string Mensaje)
         {
             InitializeComponent();
             label1.Text = Mensaje;
-            
         }
 
         private void Label1_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text != string.Empty)
-            {
-                string Mensaje = textBox1.Text;
-                Form3 F3 = new Form3(Mensaje);
-                this.Hide();
-                F3.Show();
-            }
         }
     }
 }
